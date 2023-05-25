c63e88826bc40049f58f0349529940f6deac376a
Karim Altir <mhdkarim48850@gmail.com>
2023-05-25 14:52:29 +0200

    deleted the welcome page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,24 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+            
+    <!-- Styles -->
+    <link rel="stylesheet" href="/style.css">
+    <title>Valve</title>
+    
+</head>
+<body class="antialiased">
+    @include('navigation-menu')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
-            </div>
-        </div>
+    <div class="container">
+        @yield('content')
     </div>
-</x-app-layout>
+</body>
+</html>
