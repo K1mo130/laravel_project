bedd705af9de13973c53abc834cffb43c1e9cbec
Karim Altir <mhdkarim48850@gmail.com>
2023-05-25 15:35:15 +0200

    move the code from right into the left

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,28 +1,28 @@
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
             
-            <!-- Fonts -->
-            <link rel="preconnect" href="https://fonts.bunny.net">
-            <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
                     
-            <!-- Styles -->
-            <link rel="stylesheet" href="/style.css">
+        <!-- Styles -->
+        <link rel="stylesheet" href="/style.css">
         
-            <!-- Scripts -->
-            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-            <title>Valve</title>
-        </head>
-        <body class="antialiased">
-            @include('navigation-menu')
+        <title>Valve</title>
+    </head>
+    <body class="antialiased">
+        @include('navigation-menu')
         
-            <div class="container">
-                @yield('content')
-            </div>
-        </body>
-        </html>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
         
