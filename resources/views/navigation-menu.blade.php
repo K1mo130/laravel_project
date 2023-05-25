0084bc5db5c0cec2fc0a093aa26c120c2edf46a7
Karim Altir <mhdkarim48850@gmail.com>
2023-05-25 15:52:57 +0200

    change the url

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -2,7 +2,8 @@
     @if (Route::has('login'))
         <div>
             @auth
-                <a href="{{ url('/dashboard') }}">Dashboard</a>
+                <a href="{{ route('profile.show') }}">Profile</a>
+                <a href="{{ route('logout') }}">Log out</a>
             @else
                 <a href="{{ route('login') }}">Log in</a>
 
